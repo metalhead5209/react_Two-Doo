fcd18efacabb68e59e79c3a6c881d41f660be6ec
Aaron Rodi <metalhead5209@gmail.com>
2022-08-16 20:11:43 -0500

    added edit buttons and hidden class

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,6 +7,15 @@ const Todo = (props) => {
   return (
     <li className="todo-list-item" key={props.id} id={props.id}>
       <div className="todo-name-container">
+
+      <div className="hidden">
+        <input type="text" />
+        <div className="edit-btn-group">
+          <button>Cancel</button>
+          <button>Submit</button>
+        </div>
+      </div>
+
         <p className="todo-name">{props.name}</p>
       </div>
       <div className="todo-btn-group">
@@ -15,7 +24,6 @@ const Todo = (props) => {
         >
           Edit
         </button>
-
         <button 
         className="todo-action-btn"
         onClick={() => props.deleteTask(props.id)}
